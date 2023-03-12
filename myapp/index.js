6f36ac94523fa653ea0fdaaf44fd3a5e11f42f78
abhishek <abhishek.nt94@gmail.com>
2023-03-12 01:11:07 -0800

    hbs

--- myapp/index.js
+++ myapp/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path')
+const hbs = require('hbs');
 
 const app = express()
 //rel abs
 //console.log(path.join(__dirname, '../public'))
-const staticPath = path.join(__dirname, "../public");
+const staticPath = path.join(__dirname, "./public");
 app.use(express.static(staticPath));
+//to set the view engine
+app.set('view engine','hbs');
 //buit in middle ware
 //app.use(express.static(staticPath));
+app.get("",(req,res)=>{
+    res.render('index');
+});
 app.get("/",(req,res)=>{
     res.send("hello from the express");
 });
